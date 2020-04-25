2e66bbe6279eb98db280bc3e3bb7c74a37fa9a28
Roland McRae <mcraeroland@gmail.com>
2020-04-25 04:46:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest = 1000000
+  final_key = "blank"
+  name_hash.each do |key, number|
+    if number < lowest
+      final_key = key
+    end
+  end
 end
