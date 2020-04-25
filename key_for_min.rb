b42e2ab11f610d545c12bddec6bd8445fd5cad7a
Roland McRae <mcraeroland@gmail.com>
2020-04-25 04:55:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest = 10000000000
   final_key = "blank"
-  if name_hash = {}
+  if name_hash = nil
     return nil
   else
   name_hash.each { |key, value| final_key = key if value == name_hash.values.max }
